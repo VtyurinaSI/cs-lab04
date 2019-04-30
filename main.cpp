46468d68c0c31aafbb4ae9bf908af74cf98ab68a
VtyurinaSI <svetlana0vtyurina@gmail.com>
2019-04-30 14:23:41 +0300

    первод в 16ю

--- main.cpp
+++ main.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+void print_in_hex(uint8_t byte);//маленькая
+void print_in_hex(const void* data, size_t size);//большая
+/*
+void print_in_binary(uint8_t byte);
+void print_in_binary(const void* data, size_t size);
+*/
+
+char nibble_to_hex (uint8_t i)
+{
+    char symbols[16] = '0123456789abcdef';
+    return symbol[i];
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
     return 0;
 }
